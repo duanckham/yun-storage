d1987523d047a9437aa7a3068667a4d11e793638
noevil <noevil@foxmail.com>
2014-05-21 16:26:44 +0800

    请叫我刷屏之王

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.static(__dirname + '/sdk'));
 // CORS
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 	// res.header('Access-Control-Allow-Credentials', true);
 
 	next();
